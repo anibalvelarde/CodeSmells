fcd8868233da07b12a53dc9f538e628ca6e6b359
Anibal Velarde <anibalvelarde@gmail.com>
2018-04-17 23:58:19 -0500

    Added a test library for kicks.

---
+++ src/Code.Smells.Lib.Tests/DirtyCodeVsCleanCodeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Code.Smells.Lib.Tests
+{
+    [TestClass]
+    public class DirtyCodeVsCleanCodeTests
+    {
+        [TestMethod]
+        public void Should_HaveSameOutcome_When_RunningDirtyOrCleanCode()
+        {
+            // Arrange...
+            var instanceUnderTest = new Code.Smells.Lib.Examples.DirtyCodeVsCleanCode();
+
+            // Act...
+            var dirtyCodeOutcome = instanceUnderTest.DirtyCodeExample();
+            var cleanCodeOutcome = instanceUnderTest.CleanCodeExample();
+
+            // Assert...
+            Assert.AreEqual(dirtyCodeOutcome, cleanCodeOutcome);
+        }
+    }
+}
---
+++ src/Code.Smells.Lib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Code.Smells.Lib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aon")]
+[assembly: AssemblyProduct("Code.Smells.Lib.Tests")]
+[assembly: AssemblyCopyright("Copyright © Aon 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7271304-2616-460b-92f5-630ee78a1335")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
